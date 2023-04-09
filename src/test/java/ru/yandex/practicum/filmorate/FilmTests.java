3777ae695399f98fa75f313bc0f30324d63e0916
Alex <MacMorphling@yandex.ru>
2023-04-10 01:34:59 +0300

    Коммит перед слиянием

--- src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.expection.ValidationException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
