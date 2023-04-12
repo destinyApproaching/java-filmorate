39323770c04a79e3febedc358d302395eb6981f1
Alex <MacMorphling@yandex.ru>
2023-04-12 18:26:59 +0300

    ТЗ10 №2

--- src/main/java/ru/yandex/practicum/filmorate/comparator/FilmComparator.java
+++ src/main/java/ru/yandex/practicum/filmorate/comparator/FilmComparator.java
@@ -4,7 +4,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Comparator;
 
-public class FilmComparator implements Comparator<Film>{
+public class FilmComparator implements Comparator<Film> {
     @Override
     public int compare(Film o1, Film o2) {
         return -(o1.getLikes().size() - o2.getLikes().size());
